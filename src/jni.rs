7355bf21c5eef51f63e8a012876addc4cde282be
name <email@e.com>
2025-06-06 17:35:52 +0800

    update: jni call

--- src/jni.rs
+++ src/jni.rs
@@ -1,10 +1,9 @@
+use anyhow::Ok;
 use jni::{
-    JNIEnv,
-    objects::{GlobalRef, JClass, JFieldID},
-    sys::jfieldID,
+    objects::{GlobalRef, JClass, JFieldID, JMethodID, JObject, JStaticFieldID, JStaticMethodID, JValueGen}, signature::{JavaType, ReturnType}, sys::{jfieldID, jmethodID, jvalue}, JNIEnv
 };
 use mini_moka::sync::Cache;
-use std::{fmt::format, sync::LazyLock};
+use std::sync::LazyLock;
 
 use crate::{error::Result, throw};
 
@@ -22,18 +21,192 @@ pub static STATIC_FIELD_CACHE: LazyLock<Cache<StaticFieldKey, usize>> =
 pub static STATIC_METHOD_CACHE: LazyLock<Cache<StaticMethodKey, usize>> =
     LazyLock::new(|| Cache::new(30));
 
-#[inline]
-pub fn get_jni_field_id(key: FieldKey, _env: &JNIEnv) -> Result<JFieldID> {
-    let cache_value = FIELD_CACHE.get(&key);
-    if let Some(id) = cache_value {
-        let result = unsafe {
-            let id = id as jfieldID;
-            JFieldID::from_raw(id)
-        };
-        return Ok(result);
+pub struct SpStaticField {
+    cache: StaticFieldKey,
+    name: String,
+    sig: String,
+}
+
+impl SpStaticField {
+    pub fn new(key: StaticFieldKey, name: &str, return_type: &SpType) -> Self {
+        Self {
+            cache: key,
+            name: name.to_string(),
+            sig: return_type.to_string(),
+        }
     }
 
-    throw("no cache")
+    pub fn init(&self, env: &mut JNIEnv, jclass: &JClass) -> Result<()> {
+        if STATIC_FIELD_CACHE.contains_key(&self.cache) {
+            return Ok(());
+        }
+        let raw_id = env
+            .get_field_id(jclass, &self.name, &self.sig)?
+            .into_raw();
+        STATIC_FIELD_CACHE.insert(self.cache, raw_id as usize);
+        Ok(())
+    }
+
+    pub fn call<'s>(
+        &self,
+        env: &'s mut JNIEnv,
+        class: &JClass,
+        ret: JavaType,
+    ) -> Result<JValueGen<JObject<'s>>> {
+        let field_id = match FIELD_CACHE.get(&self.cache) {
+            Some(id) => unsafe { JStaticFieldID::from_raw(id as jfieldID) },
+            None => return throw("no method cache"),
+        };
+        let result = env.get_static_field_unchecked(class, field_id, ret)?;
+        Ok(result)
+    }
+}
+
+pub struct SpField {
+    cache: FieldKey,
+    name: String,
+    sig: String,
+}
+
+impl SpField {
+    pub fn new(key: FieldKey, name: &str, return_type: &SpType) -> Self {
+        Self {
+            cache: key,
+            name: name.to_string(),
+            sig: return_type.to_string(),
+        }
+    }
+
+    pub fn init(&self, env: &mut JNIEnv, jclass: &JClass) -> Result<()> {
+        if FIELD_CACHE.contains_key(&self.cache) {
+            return Ok(());
+        }
+        let raw_id = env
+            .get_field_id(jclass, &self.name, &self.sig)?
+            .into_raw();
+        FIELD_CACHE.insert(self.cache, raw_id as usize);
+        Ok(())
+    }
+
+    pub fn call<'s>(
+        &self,
+        env: &'s mut JNIEnv,
+        this: &JObject,
+        ret: ReturnType,
+    ) -> Result<JValueGen<JObject<'s>>> {
+        let field_id = match FIELD_CACHE.get(&self.cache) {
+            Some(id) => unsafe { JFieldID::from_raw(id as jfieldID) },
+            None => return throw("no method cache"),
+        };
+        let result = env.get_field_unchecked(this, field_id, ret)?;
+        Ok(result)
+    }
+}
+
+pub struct SpStaticMethod {
+    cache: StaticMethodKey,
+    name: String,
+    sig: String,
+}
+
+impl SpStaticMethod {
+    pub fn new(key: StaticMethodKey, name: &str, return_type: &SpType, args: &[SpType]) -> Self {
+        let mut all_len = return_type.get_str_len() + 2;
+        for n in args {
+            all_len += n.get_str_len();
+        }
+        let mut sig_builder = String::with_capacity(all_len);
+        sig_builder.push('(');
+        for n in args {
+            sig_builder.push_str(&n.to_string());
+        }
+        sig_builder.push(')');
+        sig_builder.push_str(&return_type.to_string());
+
+        Self {
+            cache: key,
+            name: name.to_string(),
+            sig: sig_builder,
+        }
+    }
+
+    pub fn init(&self, env: &mut JNIEnv, jclass: &JClass) -> Result<()> {
+        if STATIC_METHOD_CACHE.contains_key(&self.cache) {
+            return Ok(());
+        }
+        let raw_id = env
+            .get_static_method_id(jclass, &self.name, &self.sig)?
+            .into_raw();
+        STATIC_METHOD_CACHE.insert(self.cache, raw_id as usize);
+        Ok(())
+    }
+
+    pub fn call<'s>(
+        &self,
+        env: &'s mut JNIEnv,
+        class: &JClass,
+        args: &[jvalue],
+        ret: ReturnType,
+    ) -> Result<JValueGen<JObject<'s>>> {
+        let method_id = match STATIC_METHOD_CACHE.get(&self.cache) {
+            Some(id) => unsafe { JStaticMethodID::from_raw(id as jmethodID) },
+            None => return throw("no method cache"),
+        };
+        let result = unsafe { env.call_static_method_unchecked(class, method_id, ret, args)? };
+        Ok(result)
+    }
+}
+
+pub struct SpMethod {
+    cache: MethodKey,
+    name: String,
+    sig: String,
+}
+
+impl SpMethod {
+    pub fn new(key: MethodKey, name: &str, return_type: &SpType, args: &[SpType]) -> Self {
+        let mut all_len = return_type.get_str_len() + 2;
+        for n in args {
+            all_len += n.get_str_len();
+        }
+        let mut sig_builder = String::with_capacity(all_len);
+        sig_builder.push('(');
+        for n in args {
+            sig_builder.push_str(&n.to_string());
+        }
+        sig_builder.push(')');
+        sig_builder.push_str(&return_type.to_string());
+
+        Self {
+            cache: key,
+            name: name.to_string(),
+            sig: sig_builder,
+        }
+    }
+
+    pub fn init(&self, env: &mut JNIEnv, jclass: &JClass) -> Result<()> {
+        if METHOD_CACHE.contains_key(&self.cache) {
+            return Ok(());
+        }
+        let raw_id = env.get_method_id(jclass, &self.name, &self.sig)?.into_raw();
+        METHOD_CACHE.insert(self.cache, raw_id as usize);
+        Ok(())
+    }
+
+    pub fn call<'s>(
+        &self,
+        env: &'s mut JNIEnv,
+        this: &JObject,
+        args: &[jvalue],
+        ret: ReturnType,
+    ) -> Result<JValueGen<JObject<'s>>> {
+        let method_id = match METHOD_CACHE.get(&self.cache) {
+            Some(id) => unsafe { JMethodID::from_raw(id as jmethodID) },
+            None => return throw("no method cache"),
+        };
+        let result = unsafe { env.call_method_unchecked(this, method_id, ret, args)? };
+        Ok(result)
+    }
 }
 
 pub enum SpType {
@@ -50,6 +223,34 @@ pub enum SpType {
     Array(Box<SpType>),
 }
 
+impl SpType {
+    pub fn new_class(class: &str) -> Self {
+        let c = SpClass::from_sig(class);
+        Self::Class(c)
+    }
+
+    pub fn get_str_len(&self) -> usize {
+        match self {
+            Self::Class(class) => {
+                let len = class
+                    .class_full_path
+                    .as_deref()
+                    .unwrap_or("java/lang/Object")
+                    .len();
+                len + 2
+            }
+            Self::Array(class) => &class.get_str_len() + 1,
+            _ => 1,
+        }
+    }
+}
+
+impl Default for SpType {
+    fn default() -> Self {
+        Self::Void
+    }
+}
+
 impl ToString for SpType {
     fn to_string(&self) -> String {
         match self {
@@ -62,13 +263,17 @@ impl ToString for SpType {
             Self::Sort => "S".to_string(),
             Self::Boolean => "Z".to_string(),
             Self::Void => "V".to_string(),
-            Self::Class(class) => format!("L{};", class.class_full_path.as_deref().unwrap_or("java/lang/Object")),
+            Self::Class(class) => format!(
+                "L{};",
+                class
+                    .class_full_path
+                    .as_deref()
+                    .unwrap_or("java/lang/Object")
+            ),
             Self::Array(t) => format!("[{}", t.to_string()),
         }
     }
 }
-
-
 
 pub struct SpClass {
     cache: ClassKey,
@@ -98,8 +303,12 @@ impl SpClass {
         if self.jni_class_ref.is_some() {
             return Ok(());
         }
-        if self.cache < 0 && self.class_full_path.is_none() {
-            return throw("no class");
+        if self.cache < 0 {
+            if self.class_full_path.is_none() {
+                return throw("no class");
+            }
+        } else if CLASS_CACHE.contains_key(&self.cache) {
+            return Ok(());
         }
 
         let result = match CLASS_CACHE.get(&self.cache) {
